b51f6f21ffff028ee6b5622870a74fdb85a277aa
Akash Balasaheb Kadam <kadamakash131724@gmail.com>
2023-01-09 12:35:46 +0530

    added para

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World ! </h1>
+      <p>
+        Navigate to the GitHub Pages settings page In your web browser, navigate
+        to the GitHub repository Above the code browser, click on the tab
+        labeled "Settings" In the sidebar, in the "Code and automation" section,
+        click on "Pages" Configure the "Build and deployment" settings like
+        this: Source: Deploy from a branch Branch: Branch: gh-pages Folder: /
+        (root) Click on the "Save" button
+      </p>
     </div>
   );
 }
